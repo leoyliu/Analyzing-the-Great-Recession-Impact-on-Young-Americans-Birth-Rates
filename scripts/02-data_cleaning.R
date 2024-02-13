ae6f7fad9824bbd4ac47ad6766f34a5148324460
tengqi <75350626+dwz92@users.noreply.github.com>
2024-02-13 02:32:17 -0500

    Deleted unnecessary dataset, fixed git by reclone, fixed figure display in qmd file. Tables to be implemented.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,98 +1,86 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data
-# Author: Yuanyi (Leo) Liu
-# Date: 21 January 2024
-# Contact: leoy.liu@mail.utoronto.ca
+# Author: Yuanyi (Leo) Liu, Qi Er (Emma) Teng
+# Date: 13 February 2024
+# Contact: leoy.liu@mail.utoronto.ca, e.teng@mail.utoronto.ca
 # License: MIT
 
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
+library(here)
+library(mapdata)
 
 
 #### Cleaned data ####
-raw_data <- read_csv("inputs/data/toronto_crime_rates.csv")
 
-# Removing unwanted columns
-cleaned_data <- raw_data %>% select(-c(X_id, HOOD_ID, POPULATION_2023, geometry))
+##Figure 1##
+figure1 <- read.csv(here::here("inputs/data/fig_1.csv"))
 
-# Removing rows with NA values
-cleaned_data <- na.omit(cleaned_data)
+#Write to CSV
+write_csv(
+  x = figure1,
+  file = "outputs/data/figure_1.csv"
+)
 
-# Save data #
+##Figure 2##
+uncleaned_data <- read.csv(here::here("inputs/data/figs_2a_2b.csv"))
+
+cleaned_data <- uncleaned_data |>
+  select(brate_1519, brate_2024, brate_2529, brate_3034, brate_3539, brate_4044, year)
+
+cleaned_data <- cleaned_data |>
+  rename(
+    "br_15 to 19" = brate_1519,
+    "br_20 to 24" = brate_2024,
+    "br_25 to 29" = brate_2529,
+    "br_30 to 34" = brate_3034,
+    "br_35 to 39" = brate_3539,
+    "br_40 to 44" = brate_4044,
+  )
+
+cleaned_data <- cleaned_data |>
+  pivot_longer(cols = starts_with("br_"),
+               names_to = "age_group",
+               names_prefix = "br_",
+               values_to = "brate")
+
+#Write to CSV
 write_csv(
   x = cleaned_data,
-  file = "cleaned_toronto_crime_rates.csv"
+  file = "outputs/data/figure_2.csv"
 )
 
-# Descriptive Summaries #
-str(cleaned_data)
-summary(cleaned_data)
+##Figure 3a##
 
+uncleaned_data <- read.csv(here::here("inputs/data/fig_3.csv"))
 
+state <- map_data("state")
 
-#### Summary Table Dataset ####
+#data <- read_dta(here::here('data/nchs/nchs_births_pop_1990_2019.dta'))
 
-# Read the data
-crime_data <- read.csv(here("inputs/data/cleaned_toronto_crime_rates.csv"))
+cleaned_data <- uncleaned_data [-c(1, 12), ]
 
-# Remove unnecessary columns
-crime_data <- crime_data %>%
-  select(-grep("RATE", names(crime_data)))
+state_full <- tibble(
+  stname=cleaned_data$stname,
+  full=c("alabama", "arkansas", "arizona", "california", "colorado", "connecticut", "district of columbia", "delaware",
+         "florida", "georgia", "iowa", "idaho", "illinois", "indiana", "kansas", "kentucky", "louisiana", "massachusetts", "maryland", "maine", "michigan",
+         "minnesota", "missouri", "mississippi", "montana", "north carolina", "north dakota", "nebraska", "new hampshire", "new jersey", "new mexico",
+         "nevada", "new york", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota",
+         "tennessee", "texas", "utah", "virginia", "vermont", "washington", "wisconsin","west virginia", "wyoming"
+  )
+)
 
-# Gather the data to a long format
-crime_data_long <- crime_data %>%
-  gather(key = "Crime_Year", value = "Count", -AREA_NAME)
+cleaned_data <- cleaned_data |>
+  left_join(state_full, by = "stname") |>
+  select(-stname) |>
+  rename(stname = full)
 
-# Extract year and crime type from the 'Crime_Year' column
-crime_data_long <- crime_data_long %>%
-  separate(Crime_Year, into = c("Crime_Type", "Year"), sep = "_") %>%
-  mutate(Year = as.numeric(Year))
+merged_data <- merge(state, cleaned_data, by.x = "region", by.y = "stname", all.x = TRUE)
 
-# Aggregate the data by year and crime type
-crime_data_summary <- crime_data_long %>%
-  group_by(Crime_Type, Year) %>%
-  summarise(Total = sum(Count, na.rm = TRUE), .groups = "drop")
-
-# Creating the summary table
-summary_table <- crime_data_summary %>%
-  group_by(Crime_Type, Year) %>%
-  summarise(Total_Cases = sum(Total, na.rm = TRUE), .groups = "drop") %>%
-  spread(key = Year, value = Total_Cases)
-
-# Define a mapping for crime types
-readable_crime_types <- c('ASSAULT' = 'Assault',
-                          'AUTOTHEFT' = 'Auto Theft',
-                          'BIKETHEFT' = 'Bike Theft',
-                          'BREAKENTER' = 'Break and Enter',
-                          'HOMICIDE' = 'Homicide',
-                          'ROBBERY' = 'Robbery',
-                          'SHOOTING' = 'Shooting Incidents',
-                          'THEFTFROMMV' = 'Theft from Motor Vehicles',
-                          'THEFTOVER' = 'Major Theft')
-
-# Update the Crime_Type column
-summary_table$Crime_Type <- readable_crime_types[summary_table$Crime_Type]
-
-# Save data #
+#Write to CSV
 write_csv(
-  x = summary_table,
-  file = "summary_table.csv"
+  x = merged_data,
+  file = "outputs/data/figure_3a.csv"
 )
-
-
-
-#### Two Types Crime Dataset ####
-summary_table$Category <- ifelse(summary_table$Crime_Type %in% c("Assault", "Break and Enter", "Homicide", "Robbery", "Shooting Incidents"), "Violent", "Non-Violent")
-
-# Group data by the new Category and sum the cases for each year
-two_types_crime <- summary_table %>%
-  group_by(Category) %>%
-  summarise(across(`2014`:`2023`, ~sum(.x, na.rm = TRUE)))
-
-# Save data #
-write_csv(
-  x = two_types_crime,
-  file = "two_types_crime.csv"
-)
-
