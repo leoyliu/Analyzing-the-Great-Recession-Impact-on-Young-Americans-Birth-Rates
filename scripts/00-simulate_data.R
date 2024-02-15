3f0ab3e905a0cb03ed45975eb6954231428a57f2
tengqi <75350626+dwz92@users.noreply.github.com>
2024-02-14 21:23:00 -0500

    fixed simulation, and figure caption

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,7 +14,8 @@ library(dplyr)
 #### Simulate ####
 
 # In this simulation, we only want two sets of birth rate data for two different age groups, young and old.
-# The simulated data is derived from a normal distribution instead of a poisson distribution since birth rate can be more than just integers.
+# The simulation start with birth rate of 6 age groups.
+# The data is derived from a normal distribution instead of a poisson distribution since birth rate can be more than just integers.
 # It is ensured with the abs() that old positive birth rate is recorded.
 
 set.seed(853)
@@ -22,9 +23,25 @@ set.seed(853)
 # Initialize a tibble
 simulated_data <- tibble(
   year = c(1980:2020),
-    young = abs(rnorm(n = 41,mean = 80.5, sd=10.5)),
-  old = abs(rnorm(n = 41,mean = 25.5, sd=5.5))
+  br_1519 = abs(rnorm(n = 41,mean = 50.5, sd=10.5)),
+  br_2024 = abs(rnorm(n = 41,mean = 55.5, sd=10.5)),
+  br_2529 = abs(rnorm(n = 41,mean = 80.5, sd=5.5)),
+  br_3034 = abs(rnorm(n = 41,mean = 55.5, sd=5.5)),
+  br_3539 = abs(rnorm(n = 41,mean = 35.5, sd=5.5)),
+  br_4044 = abs(rnorm(n = 41,mean = 25.5, sd=5.5)),
 )
+
+simulated_data$young <- simulated_data |>
+  select(
+    br_1519,br_2024,br_2529
+  ) |>
+  rowMeans()
+
+simulated_data$old <- simulated_data |>
+  select(
+    br_3034,br_3539,br_4044
+  ) |>
+  rowMeans()
 
 # View the simulated data
 print(simulated_data)
